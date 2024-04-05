4f00279985d766be2dfa5eb406432b8eb0a31cfb
Miguel <mprajim860@gmail.com>
2024-04-05 12:45:06 +0200

    Validation error fix

--- DeliverUS-Backend/src/controllers/validation/OrderValidation.js
+++ DeliverUS-Backend/src/controllers/validation/OrderValidation.js
@@ -1,6 +1,16 @@
 import { check } from 'express-validator'
-import { checkRestaurantExists } from '../../controllers/validation/ProductValidation.js'
-import { Product, Order } from '../../models/models.js'
+import { Product, Order, Restaurant } from '../../models/models.js'
+
+const checkRestaurantExists = async (value, { req }) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.body.restaurantId)
+    if (restaurant === null) {
+      return Promise.reject(new Error('The restaurantId does not exist.'))
+    } else { return Promise.resolve() }
+  } catch (err) {
+    return Promise.reject(new Error(err))
+  }
+}
 
 const checkProductIsAvailable = async (value, { req }) => {
   try {
