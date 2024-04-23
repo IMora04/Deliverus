b377526a249ffe1902176d0eb251ce97fd4ee6c1
CARMEN QUESADA <carmenquesadarguez@gmail.com>
2024-04-23 18:14:34 +0200

    OrderEndPoints Creation

---
+++ DeliverUS-Frontend-Customer/src/api/OrderEndPoints.js
@@ -0,0 +1,10 @@
+import { get } from './helpers/ApiRequestsHelper'
+function getAll () {
+  return get('orders')
+}
+
+function getDetail (id) {
+  return get(`orders/${id}`)
+}
+
+export { getAll, getDetail }
